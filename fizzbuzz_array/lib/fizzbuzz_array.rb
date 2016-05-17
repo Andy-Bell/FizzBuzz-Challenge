32b318b6125893ecdcbbede72c75f42983dc52e5
apsaxena24 <apsaxena24@gmail.com>
2016-05-17 23:00:40 +0100

    few changes in spec to make it universal

--- fizzbuzz_array/lib/fizzbuzz_array.rb
+++ fizzbuzz_array/lib/fizzbuzz_array.rb
@@ -1,10 +1,14 @@
 class Fizzbuzz_array
-	def fb_array(arr)
+	def fb_array(number)
 		arr_final = []
-		arr.each do |ele|
-			if (ele % 15 == 0)
+		(1..number).each do |i|
+			if i % 15 == 0
 				arr_final << "fizzbuzz"
+			elsif i % 5 == 0
+				arr_final << "buzz"
 			end
+				
 		end
+		return arr_final
 	end
 end
--- fizzbuzz_array/spec/fizzbuzz_array_spec.rb
+++ fizzbuzz_array/spec/fizzbuzz_array_spec.rb
@@ -2,10 +2,15 @@ require 'fizzbuzz_array'
 
 describe Fizzbuzz_array do
 	describe "obtain the array containg fizzbuzz" do
-
-		it "returns fizzbuzz to array if divisible by 15" do
-			expect(subject.fb_array).to be_an_instance_of(arr_final)
+		(1..20).each do |i|
+			it 'returns fizzbuzz to the array if divisible by 15 ' + i.to_s do
+				expect(subject.fb_array(i)).to eq [1,1,2,3,5,8] if (i % 15 == 0)
+			end
+			it 'returns buzz to the array if divisible by 5 '  + i.to_s do
+				expect(subject.fb_array(i)).to eq ['buzz'] if (i % 5 == 0)
+			end
 		end
+		
 	end
 	
 end
